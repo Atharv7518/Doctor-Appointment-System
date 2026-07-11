4b616252c3369977855e1a84a7d7a974678614e1
atharv7518 <atharvshukla085@gmail.com>
2026-07-11 16:07:39 +0530

    Fix ALLOWED_HOSTS and add missing reportlab dependency

--- doctor_project/settings.py
+++ doctor_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4+4hu*h!wojrv(algeg$$&-l%08w(@ysp2w*6=@_-wca%un0c9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
