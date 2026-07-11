64279fc52aa087ff1485e80473eecff2bdd21f18
atharv7518 <atharvshukla085@gmail.com>
2026-07-11 14:30:30 +0530

    fix:version info of mysql

--- doctor_project/__init__.py
+++ doctor_project/__init__.py
@@ -1,6 +1,6 @@
 import pymysql
 
-# Add this line to trick Django's version check
-pymysql.version_info = (1, 4, 3, "final", 0)
+# Update this line to trick Django into seeing version 2.2.3 instead of 1.4.6
+pymysql.version_info = (2, 2, 3, "final", 0)
 
 pymysql.install_as_MySQLdb()
